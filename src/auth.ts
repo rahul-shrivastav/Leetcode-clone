3e0daab34b2b68d22a65aa501d92079f61d804cf
rahul shrivastava <rahulshrivastav052@gmail.com>
2024-07-20 03:06:40 +0530

    s

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,33 @@ import bcrypt from 'bcryptjs'
 import dbConnect from "./utils/dbConnect"
 import UserModel from "./model/user"
 
+import GitHub from "next-auth/providers/github"
+import type { Provider } from "next-auth/providers"
+
+
+const providers: Provider[] = [
+    GitHub,
+    Credentials({
+        credentials: { password: { label: "Password", type: "password" } },
+        authorize(c) {
+            if (c.password !== "password") return null
+            return {
+                id: "test",
+                name: "Test User",
+                email: "test@example.com",
+            }
+        },
+    }),
+]
+
+export const providerMap = providers.map((provider) => {
+    if (typeof provider === "function") {
+        const providerData = provider()
+        return { id: providerData.id, name: providerData.name }
+    } else {
+        return { id: provider.id, name: provider.name }
+    }
+})
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
         Credentials({
@@ -29,4 +56,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             },
         }),
     ],
+    pages: {
+        signIn: "/signin",
+
+    },
+    session: {
+        strategy: 'jwt'
+    },
+    secret: process.env.NextSecret
 })
