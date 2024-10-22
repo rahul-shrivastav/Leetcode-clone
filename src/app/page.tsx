e7294ac663e2afbc21c0c34add771b757b7c186f
rahul shrivastava <rahulshrivastav052@gmail.com>
2024-10-22 16:11:20 +0530

    res

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,11 @@ export default function Home() {
       <div className="w-full overflow-x-clip h-fit min-h-svh bg-black flex flex-col ">
 
         <Navbar />
-        <div className="w-full  text-white gap-2 md:gap-10 grow  flex items-center justify-center bg-black">
+        <div className="w-full  text-white gap-2 md:gap-10 grow xl:flex-row  flex flex-col-reverse items-center justify-center bg-black">
 
-          <div className=" w-4/12 border h-[85vh] py-10 shadow-slate-900 border-violet-800 border-opacity-50 shadow-2xl flex flex-col items-center justify-center mt-20">
+
+          {/* leftside */}
+          <div className=" w-[90%] border h-[85vh] py-10 xl:w-4/12 shadow-slate-900 border-violet-800 border-opacity-50 shadow-2xl flex flex-col items-center justify-center mt-20">
             <div className="font-extralight  text-2xl  ">Welcome ,</div>
             {
               //@ts-ignore
@@ -36,12 +38,9 @@ export default function Home() {
             </div>
 
           </div>
-
-          <div className=" w-7/12 h-[85vh]  flex flex-col items-center justify-center mt-20  ">
-
+          {/* rightside */}
+          <div className=" w-[90%] h-[85vh]    xl:w-7/12  flex flex-col items-center justify-center mt-20  ">
             <Metrics easy={50} med={90} hard={70} />
-
-
           </div>
 
         </div>
--- src/components/Homepage/Metrics.tsx
+++ src/components/Homepage/Metrics.tsx
@@ -22,12 +22,12 @@ const Metrics = (props: any) => {
 
 
     return (
-        <div className="  flex flex-col rounded-md items-center justify-center w-full shadow-slate-900 border h-full border-violet-800 border-opacity-50 shadow-2xl   p-10 ">
-            <div className=" 2xl:text-2xl text-4xl font-thin relative -top-10">Your Progress</div>
+        <div className="  flex flex-col rounded-md items-center justify-center    w-full shadow-slate-900 border h-full border-violet-800 border-opacity-50 shadow-2xl   p-10 ">
+            <div className=" 2xl:text-2xl  text-4xl font-thin relative -top-10 max-[600px]:bg-blue-400 text-center border border-white">Your Progress</div>
 
-            <div className="flex scale-100  min-[1495px]:scale-[0.9] h-96 items-center justify-center gap-4">
+            <div className="flex   scale-100 max-[800px]:scale-[0.8] max-[600px]:scale-[0.6]   max-[600px]:relative max-[600px]:flex-col max-[600px]:top-4 min-[1495px]:scale-[0.9] h-96 items-center justify-center gap-4">
                 {/* esy */}
-                <div id="wrapper" className="center relative -top-20">
+                <div id="wrapper" className="center relative -top-20 max-[600px]:top-0">
                     <svg className="progress blue noselect" data-progress={44} x="0px" y="0px" viewBox="0 0 776 628">
                         <path className="track" d="M723 314L543 625.77 183 625.77 3 314 183 2.23 543 2.23 723 314z"></path>
                         <path className="fill" d="M723 314L543 625.77 183 625.77 3 314 183 2.23 543 2.23 723 314z"></path>
@@ -35,7 +35,7 @@ const Metrics = (props: any) => {
                     </svg>
                 </div>
                 {/* med */}
-                <div id="wrapper" className="center scale-[1.2]">
+                <div id="wrapper" className="center scale-[1.2] max-[600px]:scale-[1.4]">
                     <svg className="progress blue2 noselect" data-progress={props.hard} x="0px" y="0px" viewBox="0 0 776 628">
                         <path className="track" d="M723 314L543 625.77 183 625.77 3 314 183 2.23 543 2.23 723 314z"></path>
                         <path className="fill" d="M723 314L543 625.77 183 625.77 3 314 183 2.23 543 2.23 723 314z"></path>
@@ -44,7 +44,7 @@ const Metrics = (props: any) => {
                     </svg>
                 </div>
                 {/* hard */}
-                <div id="wrapper" className="center relative -top-20 ">
+                <div id="wrapper" className="center relative -top-20 max-[600px]:top-0">
                     <svg className="progress blue3 noselect" data-progress={props.med} x="0px" y="0px" viewBox="0 0 776 628">
                         <path className="track" d="M723 314L543 625.77 183 625.77 3 314 183 2.23 543 2.23 723 314z"></path>
                         <path className="fill" d="M723 314L543 625.77 183 625.77 3 314 183 2.23 543 2.23 723 314z"></path>
@@ -54,7 +54,7 @@ const Metrics = (props: any) => {
                 </div>
             </div>
 
-            <div className="flex 2xl:scale-90 flexj-col items-center justify-around w-full">
+            <div className="flex 2xl:scale-90 flexj-col items-center justify-around w-full max-[700px]:scale-[0.8] max-[800px]:flex-col max-[800px]:scale-[0.8] max-[602px]:relative max-[602px]:top-14  gap-3">
                 <div className=" flex items-center justify-center gap-2 border p-4 border-slate-700 ">
                     <div className=" flex items-center justify-center gap-2"><div className="w-4 h-4  bg-violet-300"></div> Easy -</div>
                     <div>{props.easy} / 90</div>
